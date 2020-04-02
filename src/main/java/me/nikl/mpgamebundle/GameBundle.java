552faf8d61dd30ec33e52f580712ff859a804b9f
Niklas Eicker <git@nikl.me>
2020-04-02 22:54:52 +0200

    Update to GameBox v3.0.0-beta.2

--- src/main/java/me/nikl/mpgamebundle/GameBundle.java
+++ src/main/java/me/nikl/mpgamebundle/GameBundle.java
@@ -1,37 +1,27 @@
 package me.nikl.mpgamebundle;
 
-import me.nikl.gamebox.GameBox;
-import me.nikl.gamebox.Module;
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.JavaPlugin;
+import me.nikl.gamebox.module.GameBoxModule;
+import me.nikl.mpgamebundle.rockpaperscissors.RockPaperScissors;
+import me.nikl.mpgamebundle.tictactoe.TicTacToe;
+import me.nikl.mpgamebundle.tictactoe.TicTacToeSP;
 
 /**
  * @author Niklas Eicker
  */
-public class GameBundle extends JavaPlugin {
+public class GameBundle extends GameBoxModule {
     public static final String TIC_TAC_TOE = "tictactoe";
     public static final String ROCK_PAPER_SCISSORS = "rockpaperscissors";
     public static final String TIC_TAC_TOE_SP = "tictactoesingle";
-    private GameBox gameBox;
 
     @Override
     public void onEnable() {
-        Plugin plugin = Bukkit.getPluginManager().getPlugin("GameBox");
-        if (plugin == null || !plugin.isEnabled()) {
-            getLogger().warning(" GameBox was not found! Disabling MP-GameBundle...");
-            Bukkit.getPluginManager().disablePlugin(this);
-            return;
-        }
-        gameBox = (GameBox) plugin;
-        new Module(gameBox, TIC_TAC_TOE
-                , "me.nikl.mpgamebundle.tictactoe.TicTacToe"
-                , this, TIC_TAC_TOE, "ttt");
-        new Module(gameBox, TIC_TAC_TOE_SP
-                , "me.nikl.mpgamebundle.tictactoe.TicTacToeSP"
-                , this, TIC_TAC_TOE_SP, "ttts");
-        new Module(gameBox, ROCK_PAPER_SCISSORS
-                , "me.nikl.mpgamebundle.rockpaperscissors.RockPaperScissors"
-                , this, ROCK_PAPER_SCISSORS, "rps");
+        registerGame(TIC_TAC_TOE, TicTacToe.class, "ttt");
+        registerGame(TIC_TAC_TOE_SP, TicTacToeSP.class, "ttts");
+        registerGame(ROCK_PAPER_SCISSORS, RockPaperScissors.class, "rps");
+    }
+
+    @Override
+    public void onDisable() {
+
     }
 }
